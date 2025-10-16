ca9d75a9a42f930fd7bf9c7bc368cfd50462fae2
Borryss <115550381+Borryss@users.noreply.github.com>
2025-10-16 21:06:05 +0300

    added Login sys

---
+++ CW1/Simple_Web_App/LogingUserSystem/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'LogingUserSystem/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'LogingUserSystem/login.html', {'form': form})
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
---
+++ CW1/Simple_Web_App/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
+
+@login_required
+def main(request):
+    posts = Post.objects.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, 'main/main.html', {'form': form, 'posts': posts})
